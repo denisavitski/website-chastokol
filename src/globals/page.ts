879cf4dd6e822ef33d457f5a83c788bb2d0605d2
denisavitski <denisavitski@gmail.com>
2024-12-16 13:47:44 +0300

    feat: scroll class

--- src/globals/page.ts
+++ src/globals/page.ts
@@ -1,14 +1,37 @@
+export let scrollElement = { current: document.querySelector('#page-scroll') }
+
 function updateSize() {
   const clientWidth = document.documentElement.clientWidth
 
-  document.documentElement.style.setProperty('--inner-height', innerHeight + 'px')
+  document.documentElement.style.setProperty(
+    '--inner-height',
+    innerHeight + 'px',
+  )
 
-  document.documentElement.style.setProperty('--client-width', clientWidth + 'px')
+  document.documentElement.style.setProperty(
+    '--client-width',
+    clientWidth + 'px',
+  )
+}
+
+function scrollListener() {
+  document.documentElement.classList.toggle(
+    'page-scrolled',
+    !!scrollElement.current?.scrollTop,
+  )
+}
+
+function updateScroll() {
+  scrollElement.current?.removeEventListener('scroll', scrollListener)
+  scrollElement.current = document.querySelector('#page-scroll')
+  scrollElement.current?.addEventListener('scroll', scrollListener)
+  scrollListener()
 }
 
 addEventListener('load', () => {
   document.documentElement.classList.add('page-loaded')
   updateSize()
+  updateScroll()
 })
 
 addEventListener('resize', updateSize)
